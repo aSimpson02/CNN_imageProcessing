017b31f75ec6d6a94e78618fb3905fb8361c80d6
Annabel Simpson <149061939+aSimpson02@users.noreply.github.com>
2024-11-04 19:51:14 +0000

    plan

---
+++ main.py
@@ -0,0 +1,12 @@
+#Importing relevant libraries
+import cv2
+import numpy as np
+import pandas as pd
+
+
+#Data Preprocessing 
+#CNN Model Architetcure
+#Training the Model
+#Hyperparameter tuning and experimentation
+#Evalute and Iterate
+
