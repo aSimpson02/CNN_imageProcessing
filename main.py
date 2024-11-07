706cf389cab6d1bd66dd657938d2f522ca5936de
Annabel Simpson <149061939+aSimpson02@users.noreply.github.com>
2024-11-07 09:37:34 +0000

    plannning model architecture first

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
-#Importing relevant libraries
+#Importing relevant libraries:::
 import cv2
 import numpy as np
 import pandas as pd
 
 
-#Data Preprocessing 
+#Data Preprocessing:::
+#add in data here, augmentation here too
+
+
+#CNN Model Architetcure:::
+    #function that contains architecture
+
+    #model = sequential
+    #crop any unecessary parts of images 
+    # normalise 
+
+    #5x5 layers - depth = (24, 36, 48)
+    #3x3 layers - depth = (64, 64)
+
+
+    #for flattening + connecting layers:
+    #flatter - dense - dropout - dense - dense - dense
+    #use elu activation instead of relu activation
+
+
+    #return the model for training 
 
 
 
-#CNN Model Architetcure
+#Training the Model:::
+#functions to train the model
 
 
 
-#Training the Model
+#Hyperparameter tuning and experimentation:::
 
 
 
-#Hyperparameter tuning and experimentation
-
-
-
-#Evalute and Iterate
+#Evalute and Iterate:::
 
 
 
