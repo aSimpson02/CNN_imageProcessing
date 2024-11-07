66ce9779c3ce6b18626f54641c5cc6c06c8c8ea8
Annabel Simpson <149061939+aSimpson02@users.noreply.github.com>
2024-11-07 09:51:00 +0000

    updating

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import cv2
 import numpy as np
 import pandas as pd
 
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
+
+
 
 #Data Preprocessing:::
 #add in data here, augmentation here too
-
+def data_preprocessing(image):
+    return image
 
 #CNN Model Architetcure:::
     #function that contains architecture
+def building_model():
+    pass
 
     #model = sequential
     #crop any unecessary parts of images 
@@ -30,6 +37,8 @@ import pandas as pd
 
 #Training the Model:::
 #functions to train the model
+def training_model():
+    pass
 
 
 
